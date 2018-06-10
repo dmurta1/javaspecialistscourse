954f6c23f39dc3811954c4b2f4cb2d5065e38b8f
Daniel Murta <dmurta1@hotmail.com>
2018-06-10 18:01:49 -0300

    circular array

---
+++ circulararray/src/main/java/com/looqbox/CirculararrayApplication.java
@@ -0,0 +1,12 @@
+package com.looqbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculararrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirculararrayApplication.class, args);
+	}
+}
